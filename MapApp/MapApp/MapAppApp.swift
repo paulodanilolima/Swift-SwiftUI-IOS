60831f4a42cbae9c6066a7bdda37b19b1132ad69
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-05-19 22:11:39 -0400

    Projeto SwiftUI MapApp

---
+++ MapApp/MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Paulo Danilo Conceição Lima on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapApp/MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Paulo Danilo Conceição Lima on 19/05/23.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String?
+    let cityName: String?
+    let coordinates: CLLocationCoordinate2D?
+    let description: String?
+    let imageNames: [String]?
+    let link: String?
+}
