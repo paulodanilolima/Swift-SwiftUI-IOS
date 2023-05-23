1fb0802b913ac1f4d14ec5ffacc71008967ebf5f
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-05-23 15:32:27 -0400

    MapApp with MVVM and SwiftUI

--- MapApp/MapApp/ViewModel/LocationsViewModel.swift
+++ MapApp/MapApp/ViewModel/LocationsViewModel.swift
@@ -26,6 +26,9 @@ class LocationsViewModel: ObservableObject {
     // Show list of locations
     @Published var showLocationsList: Bool = false
     
+    // Show location Detail via sheet
+    @Published var sheetLocation: Location? = nil
+    
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
