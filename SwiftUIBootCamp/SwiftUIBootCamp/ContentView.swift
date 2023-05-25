97df6fdbce401018fbce2410b4ac13e8de7b5fc6
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-05-24 21:07:55 -0400

    SwiftUI BootCamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 24/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBootCamp/SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
