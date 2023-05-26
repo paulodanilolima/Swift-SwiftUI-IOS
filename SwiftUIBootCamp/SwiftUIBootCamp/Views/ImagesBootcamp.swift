584dd1a307835991473d908ecb08185d3697e3d8
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-05-26 00:38:38 -0400

    SwiftUI Bootcamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/ImagesBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  ImagesBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 26/05/23.
+//
+
+import SwiftUI
+
+struct ImagesBootcamp: View {
+    var body: some View {
+       Image("books")
+//            .renderingMode(.template)
+            .resizable()
+            .frame(width: 300, height: 300)
+            .foregroundColor(.red)
+//            .aspectRatio(contentMode: .fit)
+            .scaledToFit()
+//            .scaledToFill()
+//            .clipped()
+//            .cornerRadius(150)
+//            .clipShape(
+//                Circle()
+//                    Ellipse()
+                
+//            )
+    }
+}
+
+struct ImagesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagesBootcamp()
+    }
+}
