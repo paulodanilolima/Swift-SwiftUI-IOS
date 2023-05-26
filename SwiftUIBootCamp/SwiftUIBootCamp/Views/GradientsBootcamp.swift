a69724929d7c126c156f9c679c6a1db64fee7802
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-05-25 23:30:02 -0400

    SwiftUI Bootcamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/GradientsBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  GradientsBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 25/05/23.
+//
+
+import SwiftUI
+
+struct GradientsBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(
+//                Color.red
+//                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+//                               startPoint: .topLeading,
+//                               endPoint: .bottom)
+//                RadialGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+//                               center: .topLeading,
+//                               startRadius: 5,
+//                               endRadius: 200)
+                
+                AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+                                center: .topLeading,
+                                angle: .degrees(180 + 45))
+            )
+            .frame(width: 300, height: 200)
+    }
+}
+
+struct GradientsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientsBootcamp()
+    }
+}
