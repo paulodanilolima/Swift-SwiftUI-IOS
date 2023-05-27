45a82eaf7cc2ceb9a56bfc00194f65cd9e8ff3b3
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-05-27 00:13:15 -0400

    SwiftUI Bootcamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/SpacerBootcamp.swift
@@ -0,0 +1,74 @@
+//
+//  SpacerBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 26/05/23.
+//
+
+import SwiftUI
+
+struct SpacerBootcamp: View {
+    var body: some View {
+//        HStack(spacing: 0) {
+//
+//            Spacer(minLength: 0)
+//                .frame(height: 10)
+//                .background(Color.orange)
+//
+//            Rectangle()
+//                .frame(width: 50, height: 50)
+//
+//            Spacer()
+//                .frame(height: 10)
+//                .background(Color.orange)
+//
+//            Rectangle()
+//                .fill(Color.red)
+//                .frame(width: 50, height: 50)
+//
+//            Spacer()
+//                .frame(height: 10)
+//                .background(Color.orange)
+//
+//            Rectangle()
+//                .fill(Color.green)
+//                .frame(width: 50, height: 50)
+//
+//            Spacer(minLength: 0)
+//                .frame(height: 10)
+//                .background(Color.orange)
+//        }
+//        .background(Color.yellow)
+////        .padding(.horizontal, 200)
+//        .background(Color.blue)
+        VStack {
+            HStack {
+                Image(systemName: "xmark")
+                Spacer()
+//                    .frame(height: 10)
+//                    .background(Color.orange)
+                
+                Image(systemName: "gear")
+            }
+            .font(.title)
+//            .background(Color.yellow)
+            .padding(.horizontal)
+//            .background(Color.blue)
+            
+            Spacer()
+//                .frame(width: 10)
+//                .background(Color.orange)
+            
+            Rectangle()
+                .frame(height: 55)
+        }
+        
+    }
+      
+}
+
+struct SpacerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SpacerBootcamp()
+    }
+}
