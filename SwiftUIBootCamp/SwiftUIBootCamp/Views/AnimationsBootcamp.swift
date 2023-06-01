e1412fc334f2f5810ba7d29af4fca185f080fafb
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-05-31 22:30:53 -0400

    SwiftUI BootCamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/AnimationsBootcamp.swift
@@ -0,0 +1,39 @@
+//
+//  AnimationsBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 31/05/23.
+//
+
+import SwiftUI
+
+struct AnimationsBootcamp: View {
+    @State var isAnimated: Bool = false
+    var body: some View {
+        VStack {
+            Button("Button") {
+                    isAnimated.toggle()
+            }
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25.0)
+                .fill(isAnimated ? Color.red : Color.green)
+                .frame(width: isAnimated ? 100 : 300,
+                       height: isAnimated ? 100 : 300)
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0 ))
+                .offset(y: isAnimated ? 300 : 0)
+                .animation(Animation
+                    .default
+                    .repeatForever(autoreverses: true))
+                       
+            Spacer()
+        }
+    }
+}
+
+struct AnimationsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsBootcamp()
+    }
+}
