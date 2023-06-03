736197d63d751986852d0566e3145a31f2522725
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-06-03 15:45:17 -0400

    SwiftUI Bootcamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/PopOverBootcamp.swift
@@ -0,0 +1,76 @@
+//
+//  PopOverBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 03/06/23.
+//
+
+import SwiftUI
+
+struct PopOverBootcamp: View {
+    
+    @State var showNewScreen: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.orange
+                .ignoresSafeArea()
+            
+            VStack {
+                Button("BUTTON") {
+                    showNewScreen.toggle()
+                }
+                .font(.largeTitle)
+                Spacer()
+            }
+//            ZStack {
+//                if showNewScreen {
+//                    NewScreen(showNewScreen: $showNewScreen)
+//                        .padding(.top, 100)
+//                        .transition(.move(edge: .bottom))
+//                        .animation(.spring())
+//            }
+//
+//        }.zIndex(2.0)
+//        .sheet(isPresented: $showNewScreen) {
+//            NewScreen()
+//        }
+            
+            NewScreen(showNewScreen: $showNewScreen)
+                .padding(.top, 100)
+                .offset(y: showNewScreen ? 0 : UIScreen.main.bounds.height)
+                .animation(.spring())
+       
+        }
+    }
+}
+
+struct NewScreen: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var showNewScreen: Bool
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Color.purple
+                .ignoresSafeArea()
+            
+            Button {
+//                presentationMode.wrappedValue.dismiss()
+                showNewScreen.toggle()
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding(20)
+            }
+
+        }
+    }
+}
+
+struct PopOverBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PopOverBootcamp()
+    }
+}
