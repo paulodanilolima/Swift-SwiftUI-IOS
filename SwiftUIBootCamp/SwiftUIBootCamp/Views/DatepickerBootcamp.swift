2ba2548fe06ad0289319d1e370442131b20877be
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-06-12 21:10:53 -0400

    SwiftUI Bootcamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/DatepickerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  DatepickerBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 12/06/23.
+//
+
+import SwiftUI
+
+struct DatepickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DatepickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DatepickerBootcamp()
+    }
+}
