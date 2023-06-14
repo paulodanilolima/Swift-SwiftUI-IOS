24028b180d6fed71597adbb8fcf894eca740f56a
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-06-13 22:04:15 -0400

    swiftUI Bootcamp

--- SwiftUIBootCamp/SwiftUIBootCamp/Views/DatepickerBootcamp.swift
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/DatepickerBootcamp.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct DatepickerBootcamp: View {
+    
+    @State var selectedDate: Date = Date()
+    let statingDate: Date = Calendar.current.date(from: DateComponents(year: 2018)) ?? Date()
+    let endingDate: Date = Date()
+    
+    
+    var dateFormatter: DateFormatter {
+        let formater = DateFormatter()
+        formater.dateStyle = .short
+        formater.timeStyle = .short
+        return formater
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Selecte Date is:")
+        Text(dateFormatter.string(from: selectedDate))
+        VStack{
+            //        DatePicker("Select a Date", selection: $selectedDate)
+            //        DatePicker("Select a date", selection: $selectedDate,
+            //                   displayedComponents: [.date, .hourAndMinute])
+                    DatePicker("Select", selection: $selectedDate, in: statingDate...endingDate, displayedComponents: [.date]
+                    )
+                        .accentColor(Color.red)
+                        .datePickerStyle(
+                            CompactDatePickerStyle()
+            //                GraphicalDatePickerStyle()
+            //                WheelDatePickerStyle()
+                        )
+        }
+
     }
 }
 
