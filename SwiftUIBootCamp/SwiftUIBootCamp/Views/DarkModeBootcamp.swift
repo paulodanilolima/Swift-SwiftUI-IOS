88a46e2164c8e1b1537300f0fe753ee9497bb436
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-06-17 20:12:18 -0400

    swiftUI Bootcamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/DarkModeBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  DarkModeBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 17/06/23.
+//
+
+import SwiftUI
+
+struct DarkModeBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DarkModeBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DarkModeBootcamp()
+    }
+}
