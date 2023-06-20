7e1fe1f10109102924c8106051aec71e72f4b8cb
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-06-19 23:51:10 -0400

    SwiftUI Bootcamp

--- SwiftUIBootCamp/SwiftUIBootCamp/Views/DarkModeBootcamp.swift
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/DarkModeBootcamp.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct DarkModeBootcamp: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack {
+                    Text("this text is PRIMARY")
+                        .foregroundColor(.primary)
+                    Text("This text is SECONDARY")
+                        .foregroundColor(.secondary)
+                    Text("This text is Black")
+                        .foregroundColor(.black)
+                    Text("This text is White")
+                        .foregroundColor(.white)
+                    Text("This text is Red")
+                        .foregroundColor(.red)
+                    Text("This text is Globaly Adaptive")
+                        .foregroundColor(Color("CustomColor"))
+                    
+                    Text("This color is locally adaptive")
+                        .foregroundColor(colorScheme == .light ? .green : .yellow)
+                }
+            }
+            .navigationTitle("Dark Mode Bootcamp")
+        }
     }
 }
 
