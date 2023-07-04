567dd7cd1c58e376bf3cbba7025bd22f38935d2d
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2023-07-03 23:24:52 -0400

    SwiftUI Bootcamp

---
+++ SwiftUIBootCamp/SwiftUIBootCamp/Views/TextSelectionBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Paulo Danilo Conceição Lima on 03/07/23.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp()
+    }
+}
